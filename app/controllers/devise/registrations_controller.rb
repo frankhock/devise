d6fc1367b855fcc9503a1653c6495ba151d93c3d
Hancock <hancock@creativekode.com>
2013-06-05 17:45:42 -0400

    added bind

--- app/controllers/devise/registrations_controller.rb
+++ app/controllers/devise/registrations_controller.rb
@@ -12,6 +12,7 @@ class Devise::RegistrationsController < DeviseController
   def create
     build_resource(sign_up_params)
 
+    binding.pry
     if resource.save
       if resource.active_for_authentication?
         set_flash_message :notice, :signed_up if is_navigational_format?
