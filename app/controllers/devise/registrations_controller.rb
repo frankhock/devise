7fbae92985ed0712602f47122b6884262730fff5
Hancock <hancock@creativekode.com>
2013-06-05 17:48:43 -0400

    added bind

--- app/controllers/devise/registrations_controller.rb
+++ app/controllers/devise/registrations_controller.rb
@@ -12,7 +12,7 @@ class Devise::RegistrationsController < DeviseController
   def create
     build_resource(sign_up_params)
 
-    binding.pry
+
     if resource.save
       if resource.active_for_authentication?
         set_flash_message :notice, :signed_up if is_navigational_format?
@@ -24,6 +24,7 @@ class Devise::RegistrationsController < DeviseController
         respond_with resource, :location => after_inactive_sign_up_path_for(resource)
       end
     else
+      binding.pry
       clean_up_passwords resource
       respond_with resource
     end
